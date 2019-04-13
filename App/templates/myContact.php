4615187ccfa11a1f117800e81b00bc02c86a8aa8
AlekseyDvornikov1 <lesha.dvornikov1@gmail.com>
2019-04-13 16:07:46 +0300

    исправлено добавление нового инпута

--- App/templates/myContact.php
+++ App/templates/myContact.php
@@ -1,3 +1,4 @@
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -80,7 +81,7 @@
                         ?>
                         <div class="form-group" id="emails">
                             <div class="controls">
-                                <select name="publish_email[]" >
+                                <select name="publish_email[]" id="pub">
                                     <option value="on" <?php if($email->publish == 'on'): echo 'selected'; endif;?>>on</option>
                                     <option value="off" <?php if($email->publish != 'on'): echo 'selected'; endif;?>>off</option>
                                 </select> <label for="pub">Publish field</label>
@@ -106,30 +107,29 @@
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="App/js/jquery-3.3.1.min.js"> </script>
 <script>
-    $(document)
-        .ready(function () {
-            const variant = $('#phone')
-                .clone(true);
-            const variant2 = $('#emails')
-                .clone(true);
-            $('#add')
-                .click(function () {
-                    $(variant)
-                        .clone(true)
-                        .appendTo('#variants')
-                        .fadeIn('slow')
-                        .focus();
-                });
-            $('#add2')
-                .click(function () {
-                    $(variant2)
-                        .clone(true)
-                        .appendTo('#variants2')
-                        .fadeIn('slow')
-                        .focus();
-
-                });
+    $(document).ready(function() {
+        const variant = $('#phone');
+        $('#add').click(function() {
+            variant.clone(true)
+                .appendTo('#variants')
+                .fadeIn('slow')
+                .find('input[type=text]')
+                .val('')
+                .focus();
         });
+    });
+    $(document).ready(function() {
+        const variant2 = $('#emails');
+        $('#add2').click(function() {
+            variant2.clone(true)
+                .appendTo('#variants2')
+                .fadeIn('slow')
+                .find('input[type=email]')
+                .val('')
+                .focus();
+        });
+    });
+
 </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
